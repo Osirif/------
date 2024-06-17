38ca0b8db55fb002c5b49679dde08943e5800f52
Osirif <103280479+Osirif@users.noreply.github.com>
2024-06-17 21:20:30 +0500

    FINAL

--- js/header__burger.js
+++ js/header__burger.js
@@ -6,10 +6,13 @@ if(window.scrollY > 100){
     header.style.background = "#0E0E0E"
     header__burger.style.background = "#0E0E0E"
     header__burger.style.position = "fixed"
+    header.style.position = "fixed"
+    
 }
 else{
     header.style.background = "transparent" 
     header__burger.style.background = "transparent"
     header__burger.style.position = "relative"
+    header.style.position = "relative"
 }
 }
